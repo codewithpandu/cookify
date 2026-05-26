048229ab5bc6720f30139d44a468f8c95a71a8c2
codewithpandu <pandusetiadarmawan01@gmail.com>
2026-05-26 08:03:55 +0700

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-black/30">
       <div className="p-4">
-        <p>&copy; 2026 Cookify</p>
+        <p className="text-sm">&copy; 2026 Cookify</p>
       </div>
     </footer>
   );
